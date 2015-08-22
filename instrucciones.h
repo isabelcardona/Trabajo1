e32a7e0abddfe116f41d1f7fefe445d32684340a
isabelcardona <iccardonas@uqvirtual.edu.co>
2015-08-21 20:16:15 -0500

    se ha modificado el archivo instrucciones.h

--- instrucciones.h
+++ instrucciones.h
@@ -1,5 +1,35 @@
 long int ands(long int rd, long int rm );
+/**
+*\brief funcion que multiplica
+*\param rd operando 1
+*\param rm operando 2
+*\return un entero con el resultado 
+*/
 long int eors(long int rd,long int rm );
+/**
+*\brief funcion que 
+*\param rd operando 1
+*\param rm operando 2
+*\return un entero con el resultado 
+*/
 long int movs(long int rd,long int imm );
+/**
+*\brief funcion que copia un archivo 
+*\param rd operando 1
+*\param imm operando 2
+*\return un entero con el resultado 
+*/
 long int orrs(long int rd,long int rm );
-long int subs(long int rd,long int rn,long int rm );
+/**
+*\brief funcion que 
+*\param rd operando 1
+*\param rm operando 2
+*\return un entero con el resultado 
+*/
+long int subs(long int rd,long int rm );
+/**
+*\brief funcion que resta
+*\param rd operando 1
+*\param rm operando 2
+*\return un entero con el resultado 
+*/
