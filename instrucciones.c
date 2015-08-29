193043255a7f8422f2ca2fafcccfb10685c2f342
isabelcardona <iccardonas@uqvirtual.edu.co>
2015-08-29 12:56:43 -0500

    se han agregado los comentarios

--- instrucciones.c
+++ instrucciones.c
@@ -10,6 +10,7 @@ unsigned long int ands(unsigned long int rd, unsigned long int rm)
     return rd;
 }
 
+
 unsigned long int eors(unsigned long int rd,unsigned long int rm)
 {
     rd=rd^rm;
@@ -25,6 +26,9 @@ unsigned  long int movs(unsigned long int rd, unsigned long int rd, unsigned lon
     return rd;
 
 }
+/**
+*\esta funcion mueve los archivos pero en realidad la funcion que hace es copiar
+*/
  unsigned long int orrs(unsigned long int rd,unsigned long int rm)
 {
     rd=rd|rm;
@@ -84,6 +88,21 @@ unsigned long int MUL(unsigned long int rd, unsigned long int rm);
     flags=idefbands(rd,rm);
     return rd;
 }
+
+unsigned long int LSL(unsigned long int rd,unsigned long int rm);
+{
+  rd=rd*2^rm;
+  return rd;
+
+}
+/**
+*\esta funcion desplaza el registro rd hacia la izquierda las veces que lo indique rm
+*/
+unsigned long int LSR(unsigned long int rd,unsigned long int rm);
+{
+    rd=rd/(2^rm);
+    return a;
+
 unsigned long int SBC(unsigned long int rd, unsigned long int rm, unsigned long int c);
 {
     rd=rd-rm+c;
@@ -91,6 +110,72 @@ unsigned long int SBC(unsigned long int rd, unsigned long int rm, unsigned long 
     return rd;
 }
 void NOP(unsigned long int rd, unsigned long int rm);
+
+
+}
+/**
+*\esta funcion desplaza el registro rd hacia la derecha las veces que lo indique rm pero no se multiplica, solo se divide
+*/
+unsigned long int ADC(unsigned long int rd,unsigned long int rm,unsigned long int c);
+{
+
+<<<<<<< HEAD
+    rd=rd+rm+c;
+}
+/**
+*\esta funcion realiza una suma agregandole la bandera de acarreo
+*/
+void CMN(unsigned long int rd, unsigned long int rm);
+{
+    n=rd+rm;
+}
+/**
+*\esta funcion realiza la suma aritmetica pero el resultado no lo guarda
+*/
+void CMP(unsigned long int rd,unsigned long int rm);
+{
+    n=rd-rm;
+}
+/**
+*\esta funcion realiza una resta aritmetica sin guardar el resultado, n no se retorna
+*/
+void TST(unsigned long int rd, unsigned long int rm);
+{
+    n=rd&rm;
+}
+/**
+*\esta funcion se realiza una operacion AND y su resultado se guarda en n pero no se retorna nada
+*/
+unsigned long int ADD(unsigned long int rd,unsigned long int rm);
+{
+    rd=rd+rm;
+    return rd;
+}
+/**
+*\esta funcion realiza una suma aritmetica
+*/
+unsigned long int MUL(unsigned long int rd, unsigned long int rm);
+{
+    rd=rd*rm;
+    return rd;
+}
+unsigned long int SBC(unsigned long int rd, unsigned long int rm, unsigned long int c);
+{
+    rd=rd-rm+c;
+}
+/**
+*\esta funcion realiza una resta agregandole la bandera de acarreo
+*/
+unsigned long int NOP(unsigned long int rd, unsigned long int rm);
+
+
+void REV(unsigned long int rd, unsigned long int rm);
+{
+     rd=Rm<<24;
+     rd=rd|Rm>>24;
+     rd=rd|((((rm<<8)>>24)<<8)|(((rm<<16)>>24)<<16));
+
+}
 
 
 void REV(unsigned long int rd, unsigned long int rm);
@@ -135,11 +220,21 @@ unsigned long int Bic (unsigned long int rd, unsigned long int rm)
     }
 
 
+
 void REV16(unsigned long int rd,unsigned long int rm);
 {
      rd=((rm<<24)>>16)|((rm<<16)>>24);
      rm=rd|(((rm>>24)<<16)|((rm>>16)<<24));
-     flags=idefbands(rd,rm);
+
+}
+unsigned long int ROR(unsigned long int rd, unsigned long int rm, unsigned long int c)
+
+{
+    unsigned long int tem, tem1;
+    tem=rm>>c;
+    tem1=rm<<(32−c);
+    c=tem|tem1;
+
 }
 unsigned long int ROR(unsigned long int rd, unsigned long int rm, unsigned long int c)
 {
@@ -149,11 +244,21 @@ unsigned long int ROR(unsigned long int rd, unsigned long int rm, unsigned long 
     c=tem|tem1;
     flags=idefbands(rd,rm);
     return c;
+
 }
 
 unsigned long int BIC(unsigned long int rd, unsigned long int rm);
 {
      rd&=~rm;
+
+    return rd;
+}
+/**
+*\esta funcion realiza una operacion AND entre un registro y otro
+*/
+unsigned long int MVN(unsigned long int rd,unsigned long int rm);
+{
+    rd=~rm;
      flags=idefbands(rd,rm);
     return rd;
 }
@@ -162,10 +267,12 @@ unsigned long int MVN(unsigned long int rd,unsigned long int rm);
 {
     rd=~rm;
     flags=idefbands(rd,rm);
+
     return rd;
 }
 
  unsigned long int ASR(unsigned long int rd,unsigned long int rm, unsigned long int c);
+
 {
     unsigned int tem;
     tem=~0>>(32-c);
--- instrucciones.h
+++ instrucciones.h
@@ -34,27 +34,118 @@ unsigned long int subs(unsigned long int rd,unsigned long int rm );
 *\return un entero con el resultado
 */
 unsigned long int LSLS(unsigned long int rd,unsigned long int rm);
+/**
+*\brief funcion que desplaza logica/ hacia la izquierda
+*\param rd operando 1
+*\param rm operando 2
+*\return un entero con el resultado
+*/
 unsigned long int LSR(unsigned long int rd,unsigned long int rm);
+/**
+*\brief funcion que desplaza logica/ hacia la derecha
+*\param rd operando 1
+*\param rm operando 2
+*\return un entero con el resultado
+*/
 unsigned long int ADC(unsigned long int rd,unsigned long int rm,unsigned long int c);
+/**
+*\brief funcion que suma con carry
+*\param rd operando 1
+*\param rm operando 2
+*\param c operando  3
+*\return un entero con el resultado
+*/
 void CMN(unsigned long int rd, unsigned long int rm);
+/**
+*\brief funcion que suma sin guardar resultado
+*\param rd operando 1
+*\param rm operando 2
+*\no retorna
+*/
 void CMP(unsigned long int rd,unsigned long int rm);
+/**
+*\brief funcion que resta sin guardar resultado
+*\param rd operando 1
+*\param rm operando 2
+*\no retorna
+*/
 void TST(unsigned long int rd, unsigned long int rm);
+/**
+*\brief funcion que hace suma lógica sin guardar
+*\param rd operando 1
+*\param rm operando 2
+*\no retorna
+*/
 unsigned long int ADD(unsigned long int rd,unsigned long int rm);
+/**
+*\brief funcion que suma
+*\param rd operando 1
+*\param rm operando 2
+*\return un entero con el resultado
+*/
 unsigned long int MUL(unsigned long int rd, unsigned long int rm);
+/**
+*\brief funcion que multiplica
+*\param rd operando 1
+*\param rm operando 2
+*\return un entero con el resultado
+*/
 unsigned long int SBC(unsigned long int rd, unsigned long int rm, unsigned long int c);
+/**
+*\brief funcion que resta con carry
+*\param rd operando 1
+*\param rm operando 2
+*\param c operando  3
+*\return un entero con el resultado
+*/
 void NOP(unsigned long int rd, unsigned long int rm);
+/**
+*\brief funcion negadora
+*\param rd operando 1
+*\param rm operando 2
+*\no retorna
+*/
 void REV(unsigned long int rd, unsigned long int rm);
+/**
+*\brief funcion que cambia orden de bytes
+*\param rd operando 1
+*\param rm operando 2
+*\no retorna
+*/
 void REV16(unsigned long int rd,unsigned long int rm);
+/**
+*\brief funcion que cambia orden cada halfword de 16 bytes
+*\param rd operando 1
+*\param rm operando 2
+*\no retorna
+*/
 unsigned long int ROR(unsigned long int rd, unsigned long int rm, unsigned long int c);
+/**
+*\brief funcion que rota hacia la derecha
+*\param rd operando 1
+*\param rm operando 2
+*\param c operando  3
+*\return un entero con el resultado
+*/
 unsigned long int BIC(unsigned long int rd, unsigned long int rm);
+/**
+*\brief funcion que realiza operacion and entre un registro y el complemento de otro
+*\param rd operando 1
+*\param rm operando 2
+*\return un entero con el resultado
+*/
 unsigned long int MVN(unsigned long int rd,unsigned long int rm);
+/**
+*\brief funcion que guarda el complemento de un registro
+*\param rd operando 1
+*\param rm operando 2
+*\return un entero con el resultado
+*/
 unsigned long int ASR(unsigned long int rd,unsigned long int rm, unsigned long int c);
-
-unsigned long int ROR (unsigned long int rd, unsigned long int rm, unsigned long int c):
-
-unsigned long int Bic (unsigned long int rd, unsigned long int rm);
-
-unsigned long int Mvn(unsigned long int rd,unsigned long int rm);
-
- unsigned long int ASR (unsigned long int rd,unsigned long int rm, unsigned long int c)
+/**
+*\brief funcion que hace desplazamiento aritmetico hacia la derecha
+*\param rd operando 1
+*\param rm operando 2
+*\return un entero con el resultado
+*/
 
