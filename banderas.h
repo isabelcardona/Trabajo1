4c983fa1133d8bd271bee2b61b6a0b2a9380ac5c
danielzambrano <dfzambranov@uqvirtual.edu.co>
2015-09-18 21:29:02 -0500

    correccion de los archivos del proyecto

--- banderas.h
+++ banderas.h
@@ -1,2 +1,2 @@
-void fbands(uint32_t rd,uint32_t rm); //funcion de banderas
+void fbands(uint32_t rd,uint32_t rm,uint32_t* bands); //funcion de banderas
                                         // muestra las banderas sin retornar.
--- instrucciones.c
+++ instrucciones.c
@@ -4,77 +4,76 @@
 #include <stdint.h>
 
 
-uint32_t flags=0; //funcion de banderas, valor inicial
-uint32_t ANDS(uint32_t rd, uint32_t  rm)
+uint32_t ANDS(uint32_t rd, uint32_t  rm, uint32_t* bands)
 {
-    rd= rd&rm;
-    flags=fbands(rd,rm);
+    rd=rd&rm;
+    fbands(rd,rm,bands);
     return rd;
 }
 
 
-uint32_t EORS(uint32_t rd,uint32_t rm)
+uint32_t EORS(uint32_t rd,uint32_t rm, uint32_t* bands)
 {
     rd=rd^rm;
-    flags=fbands(rd,rm);
+    fbands(rd,rm,bands);
     return rd;
 }
 
 
-uint32_t MOVS(uint32_t rd,uint32_t imm)  //funcion que copia archivo en otro
+uint32_t MOVS(uint32_t rd,uint32_t imm, uint32_t* bands)  //funcion que copia archivo en otro
 {
     rd=imm;
-    flags=fbands(rd,imm);
+    fbands(rd,imm,bands);
     return rd;
 
 }
 
- uint32_t ORRS(uint32_t rd,uint32_t rm)
+ uint32_t ORRS(uint32_t rd,uint32_t rm, uint32_t* bands)
 {
     rd=rd|rm;
-    flags=fbands(rd,rm);
+    fbands(rd,rm,bands);
     return rd;
 }
 
 
-uint32_t SUBS(uint32_t rd,uint32_t rm)
+uint32_t SUBS(uint32_t rd,uint32_t rm, uint32_t* bands)
 {
     rd-=rm;
-    flags=fbands(rd,rm);
+    fbands(rd,rm,bands);
     return rd;
 }
-uint32_t LSLS(uint32_t rd,uint32_t rm)
+uint32_t LSLS(uint32_t rd,uint32_t rm, uint32_t imm, uint32_t* bands)
 {
-  rd=rd*2^rm;
-  flags=fbands(rd,rm);
+  rd=rd<<imm;
+  fbands(rd,rm,bands);
   return rd;
 }
-uint32_t LSRS(uint32_t rd,uint32_t rm)
+uint32_t LSRS(uint32_t rd,uint32_t rm, uint32_t imm, uint32_t* bands)
 {
-    rd=rd/(2^rm);            //esta función desplaza el registro rd hacia la derecha
-    flags=fbands(rd,rm);     //las veces que lo indique rm pero no se multiplica, solo se divide.
+    rd=rm>>imm;            //esta función desplaza el registro rd hacia la derecha
+    fbands(rd,rm,bands);     //las veces que lo indique rm pero no se multiplica, solo se divide.
     return rd;
 }
 
 
-uint32_t ADDS(uint32_t rd,uint32_t rm)  //esta funcion realiza una suma aritmetica
+uint32_t ADDS(uint32_t rd,uint32_t rm, uint32_t* bands)  //esta funcion realiza una suma aritmetica
 {
     rd=rd+rm;
-    flags=fbands(rd,rm);
+    fbands(rd,rm,bands);
     return rd;
 }
-uint32_t MUL(uint32_t rd, uint32_t rm)
+uint32_t MUL(uint32_t rd, uint32_t rm, uint32_t* bands)
 {
     rd=rd*rm;
-    flags=fbands(rd,rm);
+    fbands(rd,rm,bands);
     return rd;
 }
 
 
-uint32_t SBC(uint32_t rd, uint32_t rm, uint32_t c)
+uint32_t SBC(uint32_t rd, uint32_t rm, uint32_t c, uint32_t* bands)
 {
     rd=rd-rm+c;
-    flags=fbands(rd,rm);
+    fbands(rd,rm,bands);
     return rd;
 }
 void NOP(void) //función que no realiza ninguna operacion
@@ -82,81 +81,87 @@ void NOP(void) //función que no realiza ninguna operacion
 
 }
 
-uint32_t ADCS(uint32_t rd,uint32_t rm,uint32_t c)
+uint32_t ADCS(uint32_t rd,uint32_t rm,uint32_t c, uint32_t* bands)
 {
 
-    rd=rd+rm+c;     //esta funcion realiza una suma agregandole la bandera de acarreo
+    rd=rd+rm+c; //esta funcion realiza una suma agregandole la bandera de acarreo
+    fbands(rd,rm,bands);
     return rd;
 }
 
 
 
-void CMN(uint32_t rd, uint32_t rm)
+void CMN(uint32_t rd, uint32_t rm, uint32_t* bands)
 {
     rd=rd+rm;   //esta funcion realiza la suma aritmetica pero el resultado no lo guarda
-
+    fbands(rd,rm,bands);
 }
 
-void CMP(uint32_t rd,uint32_t rm)
+void CMP(uint32_t rd,uint32_t rm, uint32_t* bands)
 {
     rd=rd-rm;  //esta funcion realiza una resta aritmetica sin guardar el resultado, n no se retorna
+    fbands(rd,rm,bands);
 }
 
 
 
-void TST(uint32_t rd, uint32_t rm)
+void TST(uint32_t rd, uint32_t rm, uint32_t* bands)
 {
     rd=rd&rm;   //funcion que realiza una operacion AND y su resultado se guarda en n pero no se retorna nada
+    fbands(rd,rm,bands);
 
 }
 
-void REV(uint32_t rd, uint32_t rm)
+void REV(uint32_t rd, uint32_t rm, uint32_t* bands)
 {
      rd=rm<<24;
      rd=rd|rm>>24;
      rd=rd|((((rm<<8)>>24)<<8)|(((rm<<16)>>24)<<16));
+     fbands(rd,rm,bands);
 
 }
 
 
-uint32_t MVN(uint32_t rd,uint32_t rm)
+uint32_t MVN(uint32_t rd,uint32_t rm, uint32_t* bands)
 {
     rd=~rm;
+    fbands(rd,rm,bands);
     return rd;
 }
 
-void REV16(uint32_t rd,uint32_t rm)
+void REV16(uint32_t rd,uint32_t rm, uint32_t* bands)
 {
      rd=((rm<<24)>>16)|((rm<<16)>>24);
      rm=rd|(((rm>>24)<<16)|((rm>>16)<<24));
+     fbands(rd,rm,bands);
 
 }
-uint32_t ROR(uint32_t rd, uint32_t rm, uint32_t c)
+uint32_t ROR(uint32_t rd, uint32_t rm, uint32_t x, uint32_t* bands)
 
 {
    uint32_t tem, tem1;
-    tem=rm>>c;
+    tem=rm>>x;
     tem1=rm<<32;
-    c=tem|tem1;
+    x=tem|tem1;
+    fbands(rd,rm,bands);
 
 }
 
 
-uint32_t BIC(uint32_t rd,uint32_t rm)
+uint32_t BIC(uint32_t rd,uint32_t rm, uint32_t* bands)
 {
      rd&=~rm;
-
+    fbands(rd,rm,bands);
     return rd;
 }
 
-uint32_t ASR(uint32_t rd,uint32_t rm, uint32_t c)
-
+uint32_t ASR(uint32_t rd,uint32_t rm, uint32_t x, uint32_t* bands)
 {
   uint32_t tem; //variable temporal
-    tem=~0>>(32-c);
-    tem=tem<<(32-c);
-    rd=rm>>c;
+    tem=~0>>(32-x);
+    tem=tem<<(32-x);
+    rd=rm>>x;
     rd=rd|tem;
-    flags=fbands(rd,rm);
+    fbands(rd,rm,bands);
     return rd;
 }
